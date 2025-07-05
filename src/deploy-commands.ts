2923a160b201dbfff54d5c0da52b0cd7fd574063
IL <isakl.revinge@gmail.com>
2025-07-05 21:38:40 +0200

    HLNA bot but better

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -6,20 +6,25 @@ const commandsData = Object.values(commands).map((command) => command.data);
 
 const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
 
+type DeployCommandsProps = {
+  guildId: string;
+};
 
-(async () => {
-    try {
-        console.log("Started refreshing application (/) commands.");
+export async function deployCommands({ guildId }: DeployCommandsProps) {
+  try {
+    console.log("Started refreshing application (/) commands.");
 
-        console.log(commandsData);
-        await rest.put(
-            Routes.applicationCommands(config.APPLICATION_ID),
-            {
-                body: commandsData,
-            }
-        );
-        console.log("Successfully reloaded application (/) commands.");
-    } catch (error) {
-        console.error(error);
-    }
-})();
+    await rest.put(
+      Routes.applicationGuildCommands(config.APPLICATION_ID, guildId),
+      {
+        body: commandsData,
+      }
+    );
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Client } from "discord.js";
+import { config } from "./config";
+import { commands } from "./commands";
+import { deployCommands } from "./deploy-commands";
+
+const client = new Client({
+  intents: ["Guilds", "GuildMessages", "DirectMessages"],
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  message.reply("Your message has been dually noted and sybau or sumthing.");
+});
+
+client.on("guildCreate", async (guild) => {
+  await deployCommands({ guildId: guild.id });
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+  const { commandName } = interaction;
+  if (commands[commandName as keyof typeof commands]) {
+    commands[commandName as keyof typeof commands].execute(interaction);
+  }
+});
+
+client.login(config.DISCORD_TOKEN);
